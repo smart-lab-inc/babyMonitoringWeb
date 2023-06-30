4513bf94518cd307e2f4b8ee9549311b36cb3557
edualt <eduardosebastianhdz503@gmail.com>
2023-06-30 13:02:31 -0600

    BM-34: Fix comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div className='font-semibold'><p>Typography</p></div>,
+    element: <div className="font-semibold"><p>Typography</p></div>,
   }
 ])
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
