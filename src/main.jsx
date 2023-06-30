2296aaeb8aec75ef72a0a07e84dc01907c83544c
edualt <eduardosebastianhdz503@gmail.com>
2023-06-30 12:45:20 -0600

    Add custom font

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello!</div>,
+    element: <div className='font-semibold'><p>Typography</p></div>,
   }
 ])
 
