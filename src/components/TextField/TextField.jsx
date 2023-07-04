884807d1f1c9b66f6b23c4ce6b1e43fbee944095
Fernando Guerrero <69869135+devrrior@users.noreply.github.com>
2023-07-04 17:34:21 -0600

    BM-32: Create Input and Label component (#6)
    * BM-32: Create TextField component
    
    * BM-32: Create Label component
    
    * BM-32: Merge Label and TextField in a component

---
+++ src/components/TextField/TextField.jsx
@@ -0,0 +1,81 @@
+import PropTypes from "prop-types";
+
+const TextField = ({ type, label, name, value, placeholder, disabled, error, errorText, multiline, rows, width, handleBlur, handleChange }) => {
+  const commonClassNames = `block rounded-md border-0 mt-2 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ${error ? "ring-red-600" : "ring-gray-300"
+    } outline-none placeholder:text-gray-500 focus:ring-1 focus:ring-inset ${error ? "focus:ring-red-600" : "focus:ring-blue-700"
+    } sm:text-sm sm:leading-6 ${disabled && "bg-slate-100"}`;
+
+  const inputClassNames = `${commonClassNames}`;
+
+  const textareaClassNames = `${commonClassNames} resize-none`;
+
+  const style = {
+    width,
+  };
+
+  return (
+    <>
+      <label className="text-sm text-medium text-stone-950" htmlFor={name}>{label}</label>
+      {multiline ? (
+        <textarea
+          className={textareaClassNames}
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          disabled={disabled}
+          rows={rows}
+          style={style}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+      ) : (
+        <input
+          className={inputClassNames}
+          type={type}
+          name={name}
+          value={value}
+          placeholder={placeholder}
+          disabled={disabled}
+          style={style}
+          onBlur={handleBlur}
+          onChange={handleChange}
+        />
+      )}
+      {errorText && <p className="mt-2 text-xs text-red-600">{errorText}</p>}
+    </>
+  );
+};
+
+TextField.propTypes = {
+  type: PropTypes.oneOf(["text", "email", "password"]),
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+  error: PropTypes.bool,
+  errorText: PropTypes.string,
+  multiline: PropTypes.bool,
+  rows: PropTypes.number,
+  width: PropTypes.string,
+  handleBlur: PropTypes.func,
+  handleChange: PropTypes.func,
+};
+
+TextField.defaultProps = {
+  type: "text",
+  label: "Label",
+  multiline: false,
+  name: null,
+  value: null,
+  placeholder: null,
+  disabled: false,
+  error: false,
+  errorText: null,
+  rows: 2,
+  width: "200px",
+  handleBlur: undefined,
+  handleChange: undefined,
+};
+
+export default TextField;
