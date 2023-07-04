08c990da11c0664f08ba525e94a0a062e38fdc1a
Fernando Guerrero <devrrior@gmail.com>
2023-07-03 23:24:05 -0600

    BM-32: Create TextField component

---
+++ src/components/TextField/TextField.stories.js
@@ -0,0 +1,96 @@
+import TextField from "./TextField";
+
+export default {
+  title: "TextField",
+  component: TextField,
+  tags: ["autodocs"],
+}
+
+export const Input = {
+  args: {
+    type: "text",
+    multiline: false,
+    name: null,
+    value: null,
+    placeholder: null,
+    disabled: false,
+    error: false,
+    errorText: null,
+    rows: 0,
+    width: "200px",
+  },
+}
+
+export const InputDisabled = {
+  args: {
+    type: "text",
+    multiline: false,
+    name: null,
+    value: null,
+    placeholder: null,
+    disabled: true,
+    error: false,
+    errorText: null,
+    rows: 0,
+    width: "200px",
+  },
+}
+
+export const InputError = {
+  args: {
+    type: "text",
+    multiline: false,
+    name: null,
+    value: null,
+    placeholder: null,
+    disabled: false,
+    error: true,
+    errorText: "Error text",
+    rows: 0,
+    width: "200px",
+  },
+}
+
+export const Multiline = {
+  args: {
+    type: "text",
+    multiline: true,
+    name: null,
+    value: null,
+    placeholder: null,
+    disabled: false,
+    error: false,
+    errorText: null,
+    rows: 2,
+    width: "200px",
+  },
+}
+
+export const MultilineDisabled = {
+  args: {
+    type: "text",
+    multiline: true,
+    name: null,
+    value: null,
+    placeholder: null,
+    disabled: true,
+    error: false,
+    errorText: null,
+    rows: 2,
+    width: "200px",
+  },
+}
+export const MultilineError = {
+  args: {
+    type: "text",
+    multiline: true,
+    name: null,
+    value: null,
+    placeholder: null,
+    disabled: false,
+    error: true,
+    errorText: "Error text",
+    rows: 2,
+    width: "200px",
+  },
+}
