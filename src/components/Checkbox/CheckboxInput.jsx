0d241a6ec19d8d11802b12f2313b9d1c96916678
Mauricio Cruz <mawcp7@gmail.com>
2023-07-04 21:56:56 -0600

    Fix checkbox props

--- src/components/Checkbox/CheckboxInput.jsx
+++ src/components/Checkbox/CheckboxInput.jsx
@@ -1,7 +1,7 @@
 import { Checkbox } from "@nextui-org/react";
 import "../../index.css";
 
-const CheckboxInput = ({ label, id, value}) => {
+const CheckboxInput = ({ label, id, value }) => {
   return (
     <Checkbox color="primary" size="md" id={id} value={value}>
       {label}
