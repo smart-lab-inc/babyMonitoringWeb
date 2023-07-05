32b813fde5b2604aa0174115d84517b648e2ca9f
Mauricio Cruz <mawcp7@gmail.com>
2023-07-04 19:57:30 -0600

    Add value prop to checkbox

--- src/components/Checkbox/CheckboxInput.jsx
+++ src/components/Checkbox/CheckboxInput.jsx
@@ -1,9 +1,9 @@
 import { Checkbox } from "@nextui-org/react";
 import "../../index.css";
 
-const CheckboxInput = ({ label, id }) => {
+const CheckboxInput = ({ label, id, value}) => {
   return (
-    <Checkbox color="primary" size="md" id={id}>
+    <Checkbox color="primary" size="md" id={id} value={value}>
       {label}
     </Checkbox>
   );
