1917024f540ec7dd00368ea722cde259ff5575d6
Fernando Guerrero <69869135+devrrior@users.noreply.github.com>
2023-07-05 23:35:07 -0600

    BM-51: Replace width CSS for width tailwind (#8)

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -1,17 +1,13 @@
 import PropTypes from "prop-types";
 
 const TextField = ({ type, label, name, value, placeholder, disabled, error, errorText, multiline, rows, width, handleBlur, handleChange }) => {
-  const commonClassNames = `block rounded-md border-0 mt-2 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ${error ? "ring-red-600" : "ring-gray-300"
+  const commonClassNames = `${width} block rounded-md border-0 mt-2 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ${error ? "ring-red-600" : "ring-gray-300"
     } outline-none placeholder:text-gray-500 focus:ring-1 focus:ring-inset ${error ? "focus:ring-red-600" : "focus:ring-blue-700"
     } sm:text-sm sm:leading-6 ${disabled && "bg-slate-100"}`;
 
   const inputClassNames = `${commonClassNames}`;
 
   const textareaClassNames = `${commonClassNames} resize-none`;
-
-  const style = {
-    width,
-  };
 
   return (
     <>
@@ -24,7 +20,6 @@ const TextField = ({ type, label, name, value, placeholder, disabled, error, err
           placeholder={placeholder}
           disabled={disabled}
           rows={rows}
-          style={style}
           onBlur={handleBlur}
           onChange={handleChange}
         />
@@ -36,7 +31,6 @@ const TextField = ({ type, label, name, value, placeholder, disabled, error, err
           value={value}
           placeholder={placeholder}
           disabled={disabled}
-          style={style}
           onBlur={handleBlur}
           onChange={handleChange}
         />
@@ -49,15 +43,15 @@ const TextField = ({ type, label, name, value, placeholder, disabled, error, err
 TextField.propTypes = {
   type: PropTypes.oneOf(["text", "email", "password"]),
   label: PropTypes.string.isRequired,
+  multiline: PropTypes.bool,
+  width: PropTypes.string,
   name: PropTypes.string.isRequired,
   value: PropTypes.string,
   placeholder: PropTypes.string,
   disabled: PropTypes.bool,
   error: PropTypes.bool,
   errorText: PropTypes.string,
-  multiline: PropTypes.bool,
   rows: PropTypes.number,
-  width: PropTypes.string,
   handleBlur: PropTypes.func,
   handleChange: PropTypes.func,
 };
@@ -66,6 +60,7 @@ TextField.defaultProps = {
   type: "text",
   label: "Label",
   multiline: false,
+  width: "w-full",
   name: null,
   value: null,
   placeholder: null,
@@ -73,7 +68,6 @@ TextField.defaultProps = {
   error: false,
   errorText: null,
   rows: 2,
-  width: "200px",
   handleBlur: undefined,
   handleChange: undefined,
 };
