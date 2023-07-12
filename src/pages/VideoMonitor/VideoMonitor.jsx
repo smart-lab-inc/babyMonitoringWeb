b6b7378c863e1015e4da213ddf15fab2a6c04fe2
Eduardo Altuzar <81114912+edualt@users.noreply.github.com>
2023-07-12 13:53:59 -0600

    BM-54: Add monitor page (#13)
    * BM-54: Add monitor page
    
    * BM-54: Fix comments

---
+++ src/pages/VideoMonitor/VideoMonitor.jsx
@@ -0,0 +1,20 @@
+import Button from "../../components/Button/Button";
+
+const videoMonitor = () => {
+  return (
+    <div className="flex font-primary flex-col items-center justify-center h-screen p-4">
+      <p className="z-40 text-3xl font-semibold">Monitoreo</p>
+
+      <div className="w-full rounded-lg h-64 bg-gray-300 mt-10 mb-4">
+
+      </div>
+      
+      <div className="flex flex-row justify-between gap-2  w-full">
+        <Button label="Salir" size="w-1/2" />
+        <Button primary label="Silenciar" size="w-1/2" />
+        </div>
+    </div>      
+  );
+};
+
+export default videoMonitor;
