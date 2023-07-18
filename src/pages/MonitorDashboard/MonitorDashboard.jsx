39b3d79ce6717b2ef1a180ff04325febd59687de
Fernando Guerrero <devrrior@gmail.com>
2023-07-18 11:31:17 -0600

    BM-61: Resolve conflicts

--- src/pages/MonitorDashboard/MonitorDashboard.jsx
+++ src/pages/MonitorDashboard/MonitorDashboard.jsx
@@ -1,13 +1,35 @@
 import NavBar from "../../components/NavBar/NavBar";
 import babySVG from "../../assets/svg/baby.svg";
-import Alert from "../../components/Alert/Alert";
 import Button from "../../components/Button/Button";
 import eyeSVG from "../../assets/svg/eye.svg";
 import chartSVG from "../../assets/svg/bar-chart.svg";
-import { Link, Navigate } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import routes from "../../consts/routes";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
-const Home = () => {
+const MonitorDashboard = () => {
+  const { id } = useParams();
+  const [socket,] = useState(io(`http://localhost:8085/monitoring?token=${localStorage.getItem("accessToken")}&monitorId=${id}`));
+  const [temperature, setTemperature] = useState("--");
+  const [humidity, setHumidity] = useState("--");
+
+  useEffect(() => {
+    socket.on("newSensorData", (data) => {
+      data.data.map((item) => {
+        if (item.name === "temperature") {
+          setTemperature(item.value);
+        } else if (item.name === "humidity") {
+          setHumidity(item.value);
+        }
+      });
+    });
+
+    return () => {
+      socket.off("newSensorData");
+    }
+  }, [socket, temperature, humidity]);
+
   return (
     <div className="font-primary">
       <NavBar />
@@ -23,22 +45,17 @@ const Home = () => {
         <div className="h-36 flex font-primary pb-4">
           <div className="w-1/2 h-full grid justify-items-center items-center border-r-2 border-r-black ">
             <div className="flex justify-center items-center flex-col">
-              <p className="text-5xl font-bold">25 °C</p>
+              <p className="text-5xl font-bold">{temperature} °C</p>
               <p className="text-2xl font-semibold my-5">Temperatura</p>
             </div>
           </div>
           <div className="w-1/2 h-full grid justify-items-center items-center">
             <div className="flex justify-center items-center flex-col">
-              <p className="text-5xl font-bold">25 °C</p>
-              <p className="text-2xl font-semibold my-5">Temperatura</p>
+              <p className="text-5xl font-bold">{humidity} %</p>
+              <p className="text-2xl font-semibold my-5">Humidity</p>
             </div>
           </div>
         </div>
-        <Alert
-          title="No hay actualizaciones"
-          message="Todo parece estar en orden"
-          type="success"
-        />
         <div className="py-4 flex flex-col gap-4">
           <Link to={routes.watchMonitor}>
             <Button primary icon={eyeSVG} label="Observar" size="w-full" />
@@ -52,9 +69,9 @@ const Home = () => {
             />
           </Link>
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 };
 
-export default Home;
+export default MonitorDashboard;
